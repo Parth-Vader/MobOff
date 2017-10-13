be5a94b4cfbe37c224997c9fae97955877eb9497
Parth <vermaparth97@gmail.com>
2017-10-13 16:33:24 +0530

    Changes to setup file

--- setup.py
+++ setup.py
@@ -4,6 +4,10 @@ setup(
         name='MobOff',
         version='0.1',
         py_modules=['moboff'],
+        description = 'Download and send videos to devices',
+        author = 'Parth Verma',
+        author_email = 'vermaparth97@gmail.com',
+        url = "https://github.com/parth-vader/moboff",
         install_requires=[
             'pushbullet.py',
             'youtube_dl',
