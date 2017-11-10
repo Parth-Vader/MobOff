a5f28ef342a9a2fb0a42a57982d505951600f6b3
Parth <vermaparth97@gmail.com>
2017-11-10 18:27:46 +0530

    Tag updated

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import find_packages
 
 setup(
         name='MobOff',
-        version='0.2',
+        version='0.2.1',
         py_modules=['moboff'],
         packages=find_packages(),
         description = 'Download youtube music and send to devices',
