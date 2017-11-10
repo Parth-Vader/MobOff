1a37d53df02942cb27794303e68fe041f73f5135
Parth <vermaparth97@gmail.com>
2017-11-10 17:38:54 +0530

    Tag updated and download link added

--- setup.py
+++ setup.py
@@ -5,13 +5,14 @@ from setuptools import find_packages
 
 setup(
         name='MobOff',
-        version='0.1',
+        version='0.2',
         py_modules=['moboff'],
         packages=find_packages(),
         description = 'Download youtube music and send to devices',
         author = 'Parth Verma',
         author_email = 'vermaparth97@gmail.com',
         url = "https://github.com/parth-vader/MobOff",
+        download_url = 'https://codeload.github.com/Parth-Vader/MobOff/tar.gz/0.2'
         license = "MIT License",
         install_requires=[
             'pushbullet.py',
